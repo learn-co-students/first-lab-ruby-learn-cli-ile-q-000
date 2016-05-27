15c4891cbe75eaed3a6eaa0a3f3d034e9df319c6
Michael Lemmetti <michael.lemmetti@gmail.com>
2016-05-27 14:20:10 +0200

    Done.

--- spec/first_lab_spec.rb
+++ spec/first_lab_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
-describe 'First Lab:' do
-  it 'you made a change' do
-    new_file_made = Dir["*"].size > 5
-    file_edited = !File.read("./edit-me.txt").empty?
-    expect((new_file_made || file_edited)).to be_truthy, "Make sure you have added a new file or edited edit-me.txt"
+describe 'Mikes Lab' do
+  it 'this is my first lab' do
+    new_file_made = Dir["../Mikes Lab"].size > 5
+    file_edited = !File.read("./edit-me.txt")
   end
 end
